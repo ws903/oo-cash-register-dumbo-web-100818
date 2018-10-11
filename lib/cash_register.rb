24244d927d17117d91d7186d6e341feede4e553d
ws903 <ws903@nyu.edu>
2018-10-10 20:05:37 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+require 'pry'
 
+class CashRegister
+	attr_accessor(:total, :discount)
+	attr_reader(:items)
+
+	def initialize(discount = 0)
+		@last = 0
+		@total = 0
+		@discount = discount
+		@items = []
+	end
+
+	def add_item(name, price, qty=1)
+		@total += price * qty
+		@last = price * qty
+		qty.times {@items.push(name)}
+	end
+
+	def void_last_transaction
+		@total -= @last
+	end
+
+	def apply_discount
+		if @discount == 0
+			return "There is no discount to apply."
+		else
+			@total = (@total*(1-(@discount/100.0))).to_i
+			return "After the discount, the total comes to $#{@total}."
+		end
+	end
+end
